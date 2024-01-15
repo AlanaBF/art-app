550e5d04dc28cca56dc4ce3fde1b0ba208bd8334
Alana Barrett-Frew <alanabarrett-frew@hotmail.com>
2024-01-15 20:02:20 +0000

    mobile touch added

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const ctx = canvas.getContext("2d");
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
 ctx.strokeStyle = "#BADA55";
 ctx.lineJoin = "round";
 ctx.lineCap = "round";
@@ -32,14 +33,7 @@ function draw(e) {
   if (hue >= 360) {
     hue = 0;
   }
-  // if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
-  //   direction = !direction;
-  // }
-  // if (direction) {
-  //   ctx.lineWidth++;
-  // } else {
-  //   ctx.lineWidth--;
-  // }
+
 }
 
 canvas.addEventListener("mousedown", (e) => {
@@ -51,6 +45,7 @@ canvas.addEventListener("mousemove", draw);
 canvas.addEventListener("mouseup", () => (isDrawing = false));
 canvas.addEventListener("mouseout", () => (isDrawing = false));
 
+
 function clearCanvas() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
@@ -58,3 +53,23 @@ function clearCanvas() {
 // Add an event listener to the clear button
 const clearButton = document.getElementById("clearButton");
 clearButton.addEventListener("click", clearCanvas);
+
+canvas.addEventListener("touchstart", (e) => {
+  e.preventDefault();
+  isDrawing = true;
+  const rect = canvas.getBoundingClientRect();
+  [lastX, lastY] = [e.touches[0].clientX - rect.left, e.touches[0].clientY - rect.top];
+
+});
+
+canvas.addEventListener("touchmove", (e) => {
+  e.preventDefault();
+  const rect = canvas.getBoundingClientRect();
+  draw({
+    offsetX: e.touches[0].clientX - rect.left,
+    offsetY: e.touches[0].clientY - rect.top,
+  });
+});
+
+canvas.addEventListener("touchend", () => (isDrawing = false));
+
